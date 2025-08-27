633cf84e426a055a6da484759931cc53c9b425bd
amanmaurya2000 <amanmaurya2000@gmail.com>
2025-08-27 10:15:47 +0000

    Updated logger

--- Document_portal/logger/custom_logging.py
+++ Document_portal/logger/custom_logging.py
@@ -1,22 +1,44 @@
 import logging
 import os
 from datetime import datetime
+import structlog
 class CustomLogger:
     def __init__(self,log_dir="logs"):
         self.log_dir = os.path.join(os.getcwd(), log_dir)
         os.makedirs(self.log_dir, exist_ok=True)
 
         log_file = os.path.join(self.log_dir, f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log")
-        log_file_path = os.path.join(self.log_dir,log_file)
+        self.log_file_path = os.path.join(self.log_dir,log_file)
+
+    def get_logger(self,name = __file__):
+        logger_name = os.path.basename(name)
+
+        file_handler = logging.FileHandler(self.log_file_path)
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(logging.Formatter("%(message)s"))
+
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        console_handler.setFormatter(logging.Formatter("%(message)s"))
 
         logging.basicConfig(
-            filename=log_file_path, 
-            level=logging.INFO, 
-            format="[ %(asctime)s ] %(levelname)s %(name)s (line:%(lineno)d) - %(message)s"
-            )
-        
-    def get_logger(self,name = __file__):
-        return logging.getLogger(os.path.basename(name))
+            level = logging.INFO,
+            handlers = [file_handler,console_handler],
+            format="%(message)s"
+        )
+
+        structlog.configure(
+            processors=[
+                structlog.processors.TimeStamper(fmt = "iso", utc= True, key = "timestamp"),
+                structlog.processors.add_log_level,
+                structlog.processors.EventRenamer(to="event"),
+                structlog.processors.JSONRenderer()
+            ],
+            logger_factory = structlog.stdlib.LoggerFactory(),
+            cache_logger_on_first_use = True,
+        )
+
+        return structlog.getLogger(logger_name)
     
 if __name__ == "__main__":
     logger = CustomLogger()
